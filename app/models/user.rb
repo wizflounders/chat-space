065117e8b94904b7d774e09a6b45d12a75d00430
Mika <wizflounder@gmail.com>
2017-05-14 17:47:28 +0900

    Added validation to user.rb
    Added validation of name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   devise :database_authenticatable, :validatable, password_length: 8..128
+
+  validates :name, presence:  {message: "を入力してください"}, uniqueness: {message: "は既に他のユーザが使用しています"}
+  validates :email, presence:  {message: "を入力してください"}, uniqueness: {message: "は既に他のユーザが使用しています"}
 end
