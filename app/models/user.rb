3ee454c73ffd8d01b4b5cafa154a0742825f48c2
Mika <wizflounder@gmail.com>
2017-05-14 16:47:18 +0900

    Changed  minimum password length (user.rb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :validatable, password_length: 8..128
 end
