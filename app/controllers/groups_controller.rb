d0524cc292cd650897f9ed571d115568e7446293
Mika <wizflounder@gmail.com>
2017-05-16 18:35:54 +0900

    Added actions of creating group's name

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,11 +4,14 @@ class GroupsController < ApplicationController
   end
 
   def create
-     group = Group.create(create_params)
+    group = Group.create(create_params)
+    group.group_users.create(user_id: current_user.id, group_id: group.id)
+
+    redirect_to :root and return
   end
 
   private
   def create_params
-    params.require(:chat_group).permit(:name)
+    params.require(:group).permit(:name)
   end
 end
