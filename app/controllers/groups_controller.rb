84fbc9b4bfaefe1e76bd0bc650f17a2d4d2c2b19
Mika <wizflounder@gmail.com>
2017-05-16 15:07:21 +0900

    Added new and create action to groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,14 @@
 class GroupsController < ApplicationController
+  def new
+   @group = Group.new
+  end
+
+  def create
+     group = Group.create(create_params)
+  end
+
+  private
+  def create_params
+    params.require(:chat_group).permit(:name)
+  end
 end
