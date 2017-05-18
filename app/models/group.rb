ba28b10fb7acdddf27c6a52f03d3de2c28baeb55
Mika <wizflounder@gmail.com>
2017-05-18 15:57:52 +0900

    Set validation to group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :group_users
+  accepts_nested_attributes_for :users
+  validates :name, presence:  {message: "を入力してください"},uniqueness: {message: "は既に使用してされています"}
 
 end
