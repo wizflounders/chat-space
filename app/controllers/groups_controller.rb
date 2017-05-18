cfa5f34f2f4f6308731f9af69603421af50356c7
Mika <wizflounder@gmail.com>
2017-05-18 15:57:16 +0900

    Added function of adding members to chat groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,14 +4,18 @@ class GroupsController < ApplicationController
   end
 
   def create
-    group = Group.create(create_params)
-    group.group_users.create(user_id: current_user.id, group_id: group.id)
+    group = Group.create(create_group_params)
+    group.save
 
+    # 自身をチェックしていない場合に自身をメンバー追加
+    unless group.user_ids.include?(current_user.id)
+      group.group_users.create(user_id: current_user.id, group_id: group.id)
+    end
     redirect_to :root and return
   end
 
   private
-  def create_params
-    params.require(:group).permit(:name)
+  def create_group_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
