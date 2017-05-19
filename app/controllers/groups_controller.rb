b104f5ecf802f7b002295faf160974279cea0dbf
Mika <wizflounder@gmail.com>
2017-05-19 14:01:38 +0900

    Fixed #create and set error messages when creating groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,14 +4,18 @@ class GroupsController < ApplicationController
   end
 
   def create
-    group = Group.create(create_group_params)
-    group.save
-
+    @group = Group.new(create_group_params)
     # 自身をチェックしていない場合に自身をメンバー追加
-    unless group.user_ids.include?(current_user.id)
-      group.group_users.create(user_id: current_user.id, group_id: group.id)
+    unless @group.user_ids.include?(current_user.id)
+      @group.group_users.new(user_id: current_user.id, group_id: @group.id)
     end
-    redirect_to :root and return
+    @group.save
+    if @group.save
+      redirect_to :root , notice: "グループが作成されました！"
+    else
+      render 'new'
+    end
+
   end
 
   private
