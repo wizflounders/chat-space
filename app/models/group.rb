e2150585924d5aed49ea4cd35a2bc43ee335ad79
Mika <wizflounder@gmail.com>
2017-05-19 14:55:56 +0900

    Deleted unnecessary code

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :group_users
-  accepts_nested_attributes_for :users
+
   validates :name, presence: true, uniqueness: true
 
 end
