91e608c0e9d086e4a0a80e56f8280f9e1629497a
Mika <wizflounder@gmail.com>
2017-05-22 17:03:58 +0900

    Cleared routes.rb
    cleared resources :groups

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:show, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update, :show]
+  resources :groups, except: [:index]
   resources :messages, only: [:index]
 
 
