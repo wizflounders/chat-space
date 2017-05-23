cf28bc269c90d13e89eea678921b1d41674ce57b
Mika <wizflounder@gmail.com>
2017-05-23 10:06:05 +0900

    Fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:show, :edit, :update]
-  resources :groups, except: [:index]
+  resources :groups, except: [:index, :destroy]
   resources :messages, only: [:index]
 end
