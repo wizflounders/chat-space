50e965f43d642ac613b36a1c988cd18fe4a15539
Mika <wizflounder@gmail.com>
2017-05-26 11:45:00 +0900

    Fixed and cleared code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,6 @@ Rails.application.configure do
     Bullet.bullet_logger = true #
     Bullet.console = true
     Bullet.rails_logger = true
-    Bullet.add_footer = true 
+    Bullet.add_footer = true
   end
-end
 end
