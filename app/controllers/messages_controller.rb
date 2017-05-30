229435da335ef1bb3d684006059cafed2e1f5856
Mika <wizflounder@gmail.com>
2017-05-30 17:11:18 +0900

    Cleared messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_index
+  before_action :set_group, :set_message
   def index
 
   end
@@ -19,9 +19,12 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:body,:image).merge(group_id: params[:group_id])
   end
 
-  def set_index
+  def set_group
     @groups = current_user.groups.includes(:messages)
     @group = Group.find(params[:group_id])
+  end
+
+  def set_message
     @message = @group.messages.new
     @messages = @group.messages.includes(:user).order(created_at: :DESC)
   end
