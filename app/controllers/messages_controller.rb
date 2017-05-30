cea4ec6f6aa107c2da7397f4d7cf05b6f521f28b
Mika <wizflounder@gmail.com>
2017-05-30 12:49:47 +0900

    Cleared messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,7 @@
 class MessagesController < ApplicationController
+  before_action :set_index
   def index
-   @groups = current_user.groups.includes(:messages)
-   @group = Group.find(params[:group_id])
-   @message = @group.messages.new
-   @messages = @group.messages.includes(:user).order(created_at: :DESC)
+
   end
 
   def create
@@ -11,13 +9,20 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to group_messages_path(@message.group), notice: "メッセージ送信成功"
     else
-      flash[:alert] = 'メッセージもしくは画像を送信してください'
-      redirect_to group_messages_path(@message.group)
+      flash.now[:alert] = 'メッセージもしくは画像を送信してください'
+      render "index"
     end
   end
 
   private
   def create_params
     params.require(:message).permit(:body,:image).merge(group_id: params[:group_id])
+  end
+
+  def set_index
+    @groups = current_user.groups.includes(:messages)
+    @group = Group.find(params[:group_id])
+    @message = @group.messages.new
+    @messages = @group.messages.includes(:user).order(created_at: :DESC)
   end
 end
