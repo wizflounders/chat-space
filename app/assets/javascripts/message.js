8873bf9701f0a1a63cab9627b10397f8ef9490a4
Mika <wizflounder@gmail.com>
2017-06-06 17:02:43 +0900

    Fixed message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,19 @@
 $(function() {
   function buildHtml(data) {
-    var user_name = $('<div class="chat-body__message-box__name">').append(data.name);
-    var message_time = $('<div class="chat-body__message-box__date">').append(data.created_at);
-    var message_body = $('<div class="chat-body__message-box__body">').append(data.body);
-    var imageClass = $('<div class="chat-body__message-box__image">');
-    if (data.image) {
-     var imageSrc = image.append("<img src=" + image + ">");
-     var insertImage = imageClass.append(imageSrc);
-   }
-    var html = $('<div class="chat-body__message-box">').append(user_name, message_time, message_body, insertImage);
-    return html;
+console.log(data)
+    if ( data.message.image === null ) { data.message.image = ''; }
+    var $html = $( `
+                <div class="chat-body__message-box">
+                    <div class="chat-body__message-box__name">${data.message.name}</div>
+                    <div class="chat-body__message-box__date">${data.message.time}</div>
+                <div class="chat-body__message-box__body">${data.message.body}
+                    <div class="chat-body__message-box__image"></br>
+                        <img src="${data.message.image}">
+                    </div>
+                </div>
+            </li>
+        ` );
+    return $html;
   }
 
   $('#new_message').on('submit', function(e) {
@@ -19,7 +23,7 @@ $(function() {
   var fileField = $('#message_image');
   var formData = new FormData($(this)[0]);
   var current_url = location.pathname;
- for(item of formData) console.log(item);
+
   $.ajax({
     type: 'POST',
     dataType:'json',
@@ -27,6 +31,7 @@ $(function() {
     data: formData,
     processData: false,
     contentType: false,
+
   })
 
   .done(function(data) {
@@ -34,7 +39,6 @@ $(function() {
     $('.chat-body').append(html);
     textField.val('');
     fileField.val('');
-
   })
   .fail(function() {
     alert('error');
